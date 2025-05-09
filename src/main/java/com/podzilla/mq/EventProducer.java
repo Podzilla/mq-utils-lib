1c7cf2d2a0dc893278083bcd305d6d435aa1aaf8
Mohamed <mohamedkhaled3082@gmail.com>
2025-05-09 16:06:31 +0300

    chore: remove lombok dependency

--- src/main/java/com/podzilla/mq/EventProducer.java
+++ src/main/java/com/podzilla/mq/EventProducer.java
@@ -3,12 +3,14 @@ package com.podzilla.mq;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 
-import lombok.AllArgsConstructor;
 
 @Component
-@AllArgsConstructor
 public class EventProducer {
     private final RabbitTemplate rabbitTemplate;
+
+    public EventProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
 
     public void publishEvent(EventMetadata eventMetadata) {
         rabbitTemplate.convertAndSend(eventMetadata.getExchange(),
